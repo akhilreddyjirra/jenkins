cf0ea36cc083f3376d5dc953172ba6ae8e0250fc
akhilreddyjirra <akhilreddyjirra@gmail.com>
2020-02-06 13:43:06 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -3,3 +3,7 @@ import src.bcgov.GitHubHelper.*
 import src.bcgov.date.*
 import src.bcgov.abcd.*
 println("TIMESTAMP")
+stage('testing lib') {
+    def TIMESTAMP = getTimeStamp();
+    echo "${TIMESTAMP}"
+}
